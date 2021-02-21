c5f1d696ebdc89f2944ecec9675e434fe52c93ef
An An <2236368544@qq.com>
2021-02-21 21:10:21 +0800

    Add margin properties for AAChart

--- lib/AAChartsLib/AAOptionsModel/AAChart.dart
+++ lib/AAChartsLib/AAOptionsModel/AAChart.dart
@@ -1,75 +1,92 @@
 import 'AAAnimation.dart';
 
-
 class AAChart {
-    String type;
-    Object backgroundColor;
-    String plotBackgroundImage;
-    String pinchType;
-    bool panning;
-    String panKey;
-    bool polar;
-    AAAnimation animation;
-    bool inverted;
-    double marginLeft;
-    double marginRight;
+  String type;
+  Object backgroundColor;
+  String plotBackgroundImage;
+  String pinchType;
+  bool panning;
+  String panKey;
+  bool polar;
+  AAAnimation animation;
+  bool inverted;
+  List<double> margin;
+  double marginTop;
+  double marginRight;
+  double marginBottom;
+  double marginLeft;
 
-     AAChart typeSet(String prop) {
-        type = prop;
-        return this;
-    }
+  AAChart typeSet(String prop) {
+    type = prop;
+    return this;
+  }
 
-     AAChart backgroundColorSet(Object prop) {
-        backgroundColor = prop;
-        return this;
-    }
+  AAChart backgroundColorSet(Object prop) {
+    backgroundColor = prop;
+    return this;
+  }
 
-     AAChart plotBackgroundImageSet(String prop) {
-        plotBackgroundImage = prop;
-        return this;
-    }
+  AAChart plotBackgroundImageSet(String prop) {
+    plotBackgroundImage = prop;
+    return this;
+  }
 
-     AAChart pinchTypeSet(String prop) {
-        pinchType = prop;
-        return this;
-    }
+  AAChart pinchTypeSet(String prop) {
+    pinchType = prop;
+    return this;
+  }
 
-     AAChart panningSet(bool prop) {
-        panning = prop;
-        return this;
-    }
+  AAChart panningSet(bool prop) {
+    panning = prop;
+    return this;
+  }
 
-     AAChart panKeySet(String prop) {
-        panKey = prop;
-        return this;
-    }
+  AAChart panKeySet(String prop) {
+    panKey = prop;
+    return this;
+  }
 
-     AAChart polarSet(bool prop) {
-        polar = prop;
-        return this;
-    }
+  AAChart polarSet(bool prop) {
+    polar = prop;
+    return this;
+  }
 
-     AAChart animationSet(AAAnimation prop) {
-        animation = prop;
-        return this;
-    }
+  AAChart animationSet(AAAnimation prop) {
+    animation = prop;
+    return this;
+  }
 
-     AAChart invertedSet(bool prop) {
-        inverted = prop;
-        return this;
-    }
+  AAChart invertedSet(bool prop) {
+    inverted = prop;
+    return this;
+  }
 
-     AAChart marginLeftSet(double prop) {
-        marginLeft = prop;
-        return this;
-    }
+  AAChart marginSet(List<double> prop) {
+    margin = prop;
+    return this;
+  }
 
-     AAChart marginRightSet(double prop) {
-        marginRight = prop;
-        return this;
-    }
+  AAChart marginTopSet(double prop) {
+    marginTop = prop;
+    return this;
+  }
 
-    Map<String, dynamic> toJson() {
+  AAChart marginRightSet(double prop) {
+    marginRight = prop;
+    return this;
+  }
+
+  AAChart marginBottomSet(double prop) {
+    marginBottom = prop;
+    return this;
+  }
+
+  AAChart marginLeftSet(double prop) {
+    marginLeft = prop;
+    return this;
+  }
+
+  Map<String, dynamic> toJson() {
     return {
       "type": this.type,
       "backgroundColor": this.backgroundColor,
