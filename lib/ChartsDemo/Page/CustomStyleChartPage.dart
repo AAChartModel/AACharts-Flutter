57f4134c61517f728fd2b45eae4a255c6eb59850
An An <2236368544@qq.com>
2022-05-11 09:53:07 +0800

    Create CustomStyleChartPage.dart

---
+++ lib/ChartsDemo/Page/CustomStyleChartPage.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomStyleChartPage extends StatelessWidget {
+  const CustomStyleChartPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Center(
+          child: Hero(
+            tag: 'imageHero',
+            child: Image.network(
+              'https://picsum.photos/250?image=9',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
