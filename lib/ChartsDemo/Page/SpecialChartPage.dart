b8781bf9fa744ca27682dd127ed0077366e351a8
An An <2236368544@qq.com>
2022-05-11 20:25:51 +0800

    Create SpecialChartPage.dart

---
+++ lib/ChartsDemo/Page/SpecialChartPage.dart
@@ -0,0 +1,31 @@
+
+import 'package:aacharts_flutter/AAChartsLib/AAChartCreator/AAChartView.dart';
+import 'package:aacharts_flutter/ChartsDemo/ChartComposer/CustomStyleChartComposer.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SpecialChartPage extends StatelessWidget {
+  const SpecialChartPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var aaChartView = AAChartView();
+
+    var aaChartModel = CustomStyleChartComposer.configureChartWithShadowStyle();
+    aaChartView.aa_drawChartWithChartModel(aaChartModel);
+
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Center(
+          child: Hero(
+              tag: 'imageHero',
+              child: aaChartView
+          ),
+        ),
+      ),
+    );
+  }
+}
