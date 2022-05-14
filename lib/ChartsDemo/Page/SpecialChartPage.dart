ce1bc81e90c911dcd7089d2303e19d684757eca1
An An <2236368544@qq.com>
2022-05-14 17:23:46 +0800

    Update SpecialChartPage.dart

--- lib/ChartsDemo/Page/SpecialChartPage.dart
+++ lib/ChartsDemo/Page/SpecialChartPage.dart
@@ -1,17 +1,35 @@
 
+import 'package:aacharts_flutter/AAChartsLib/AAChartCreator/AAChartModel.dart';
 import 'package:aacharts_flutter/AAChartsLib/AAChartCreator/AAChartView.dart';
 import 'package:aacharts_flutter/ChartsDemo/ChartComposer/CustomStyleChartComposer.dart';
+import 'package:aacharts_flutter/ChartsDemo/ChartComposer/SpecialChartComposer.dart';
+import 'package:aacharts_flutter/ChartsDemo/ChartComposer/SpecialChartComposer.dart';
+import 'package:aacharts_flutter/ChartsDemo/ChartComposer/SpecialChartComposer.dart';
+import 'package:aacharts_flutter/ChartsDemo/ChartComposer/SpecialChartComposer.dart';
+import 'package:aacharts_flutter/ChartsDemo/ChartComposer/SpecialChartComposer.dart';
+import 'package:aacharts_flutter/ChartsDemo/ChartComposer/SpecialChartComposer.dart';
+import 'package:aacharts_flutter/ChartsDemo/ChartComposer/SpecialChartComposer.dart';
+import 'package:aacharts_flutter/ChartsDemo/ChartComposer/SpecialChartComposer.dart';
+import 'package:aacharts_flutter/ChartsDemo/ChartComposer/SpecialChartComposer.dart';
+import 'package:aacharts_flutter/ChartsDemo/ChartComposer/SpecialChartComposer.dart';
+import 'package:aacharts_flutter/ChartsDemo/ChartComposer/SpecialChartComposer.dart';
+import 'package:aacharts_flutter/ChartsDemo/ChartComposer/SpecialChartComposer.dart';
+import 'package:aacharts_flutter/ChartsDemo/ChartComposer/SpecialChartComposer.dart';
+import 'package:aacharts_flutter/ChartsDemo/ChartComposer/SpecialChartComposer.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SpecialChartPage extends StatelessWidget {
-  const SpecialChartPage({Key key}) : super(key: key);
+  final String selectedType;
+
+  const SpecialChartPage({Key key, @required this.selectedType}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
     var aaChartView = AAChartView();
 
-    var aaChartModel = CustomStyleChartComposer.configureChartWithShadowStyle();
+    var aaChartModel = configureChartModelWithChartType(selectedType);
     aaChartView.aa_drawChartWithChartModel(aaChartModel);
 
     return Scaffold(
@@ -28,4 +46,25 @@ class SpecialChartPage extends StatelessWidget {
       ),
     );
   }
+
+  AAChartModel configureChartModelWithChartType(String chartType) {
+    switch (chartType) {
+      case AAChartType.column: return SpecialChartComposer.configureColumnChart();
+      case AAChartType.pie: return SpecialChartComposer.configurePieChart();
+      case AAChartType.bubble: return SpecialChartComposer.configureBubbleChart();
+      case AAChartType.scatter: return SpecialChartComposer.configureScatterChart();
+      case AAChartType.arearange: return SpecialChartComposer.configureArearangeChart();
+      case AAChartType.areasplinerange: return SpecialChartComposer.configureAreasplinerangeChart();
+      case AAChartType.columnrange: return SpecialChartComposer.configureColumnrangeChart();
+      case AAChartType.line: return SpecialChartComposer.configureStepLineChart();
+      case AAChartType.area: return SpecialChartComposer.configureStepAreaChart();
+      case AAChartType.boxplot: return SpecialChartComposer.configureBoxplotChart();
+      case AAChartType.waterfall: return SpecialChartComposer.configureWaterfallChart();
+      case AAChartType.pyramid: return SpecialChartComposer.configurePyramidChart();
+      case AAChartType.funnel: return SpecialChartComposer.configureFunnelChart();
+      case AAChartType.errorbar: return SpecialChartComposer.configureErrorbarChart();
+      default: return SpecialChartComposer.configureColumnChart();
+    }
+  }
+
 }
