edc84b0ee76f3dd3211f6a7ae5a5b3f35db5ed4f
An An <2236368544@qq.com>
2022-05-14 16:10:25 +0800

    Create AAStates.dart

---
+++ lib/AAChartsLib/AAOptionsModel/AAStates.dart
@@ -0,0 +1,130 @@
+ class AAStates {
+  AAHover hover;
+  AASelect select;
+  AAInactive inactive;
+
+  AAStates hoverSet(AAHover prop) {
+    hover = prop;
+    return this;
+  }
+
+  AAStates selectSet(AASelect prop) {
+    select = prop;
+    return this;
+  }
+
+  AAStates inactiveSet(AAInactive prop) {
+    inactive = prop;
+    return this;
+  }
+
+}
+
+ class AAHover {
+   bool enabled;
+   String borderColor;
+   double brightness;
+   String color;
+   AAHalo halo;
+   double lineWidth;
+   double lineWidthPlus;
+
+   AAHover enabledSet(bool prop) {
+     enabled = prop;
+     return this;
+   }
+
+   AAHover borderColorSet(String prop) {
+     borderColor = prop;
+     return this;
+   }
+
+   AAHover brightnessSet(double prop) {
+     brightness = prop;
+     return this;
+   }
+
+   AAHover colorSet(String prop) {
+     color = prop;
+     return this;
+   }
+
+   AAHover haloSet(AAHalo prop) {
+     halo = prop;
+     return this;
+   }
+
+   AAHover lineWidthSet(double prop) {
+     lineWidth = prop;
+     return this;
+   }
+
+   AAHover lineWidthPlusSet(double prop) {
+     lineWidthPlus = prop;
+     return this;
+   }
+ }
+
+ class AASelect {
+   bool enabled;
+   String borderColor;
+   String color;
+   AAHalo halo;
+
+   AASelect enabledSet(bool prop) {
+     enabled = prop;
+     return this;
+   }
+
+   AASelect borderColorSet(String prop) {
+     borderColor = prop;
+     return this;
+   }
+
+   AASelect colorSet(String prop) {
+     color = prop;
+     return this;
+   }
+
+    AASelect haloSet(AAHalo prop) {
+     halo = prop;
+     return this;
+   }
+ }
+
+ class AAInactive {
+   bool enabled;
+   double opacity;
+
+   AAInactive enabledSet(bool prop) {
+     enabled = prop;
+     return this;
+   }
+
+   AAInactive opacitySet(double prop) {
+     opacity = prop;
+     return this;
+   }
+ }
+
+ class AAHalo {
+   Map attributes;
+   double opacity;
+   double size;
+
+   AAHalo attributesSet(Map prop) {
+     attributes = prop;
+     return this;
+   }
+
+   AAHalo opacitySet(double prop) {
+     opacity = prop;
+     return this;
+   }
+
+   AAHalo sizeSet(double prop) {
+     size = prop;
+     return this;
+   }
+ }
+
