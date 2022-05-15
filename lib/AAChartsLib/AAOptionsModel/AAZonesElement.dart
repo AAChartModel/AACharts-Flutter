36dd070596ba5efb45964cd1be12d46a515463c0
An An <2236368544@qq.com>
2022-05-15 15:48:37 +0800

    Create AAZonesElement.dart

---
+++ lib/AAChartsLib/AAOptionsModel/AAZonesElement.dart
@@ -0,0 +1,38 @@
+import 'package:aacharts_flutter/AAChartsLib/AAOptionsModel/AAObject.dart';
+
+class AAZonesElement extends AAObject {
+  double value;
+  Object color;
+  Object fillColor;
+  String dashStyle;
+
+  AAZonesElement valueSet(double prop) {
+    value = prop;
+    return this;
+  }
+
+  AAZonesElement colorSet(Object prop) {
+    color = prop;
+    return this;
+  }
+
+  AAZonesElement fillColorSet(Object prop) {
+    fillColor = prop;
+    return this;
+  }
+
+  AAZonesElement dashStyleSet(String prop) {
+    dashStyle = prop;
+    return this;
+  }
+
+    Map<String, dynamic> toJson() {
+      return {
+        "value": this.value,
+        "color": this.color,
+        "fillColor": this.fillColor,
+        "dashStyle": this.dashStyle,
+      };
+    }
+
+  }
