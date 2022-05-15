897cd679765d85cb911a9a98266ea2cb5fb619d9
An An <2236368544@qq.com>
2022-05-15 17:50:34 +0800

    Update AALabel.dart

--- lib/AAChartsLib/AAOptionsModel/AALabel.dart
+++ lib/AAChartsLib/AAOptionsModel/AALabel.dart
@@ -2,15 +2,15 @@ import 'AAObject.dart';
  import 'AAStyle.dart';
 
 class AALabel extends AAObject  {
-   String  align;//标签的对齐方式，可用的值有 "left"、"center" 及 "right"。默认值是根据坐标轴的位置（在图表中的位置）即标签的旋转角度进行智能判断的。 默认是：center.
-   int  rotation;//标签的旋转角度 默认是：0.
-   String  text;//文字
-   String  textAlign;//文字对齐
+   String align;//标签的对齐方式，可用的值有 "left"、"center" 及 "right"。默认值是根据坐标轴的位置（在图表中的位置）即标签的旋转角度进行智能判断的。 默认是：center.
+   int rotation;//标签的旋转角度 默认是：0.
+   String text;//文字
+   String textAlign;//文字对齐
    bool useHTML;//HTML渲染
-   String  verticalAlign;//竖直对齐
+   String verticalAlign;//竖直对齐
    AAStyle style;//轴标签的 CSS 样式
-   double  x;//水平偏移
-   double  y;// 竖直偏移
+   double x;//水平偏移
+   double y;// 竖直偏移
 
    AALabel alignSet(String prop) {
     align = prop;
@@ -22,7 +22,7 @@ class AALabel extends AAObject  {
     return this;
   }
 
-   AALabel textSet(String prop) {
+  AALabel textSet(String prop) {
     text = prop;
     return this;
   }
