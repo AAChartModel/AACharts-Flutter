f8f5434e0663a5801f9362f61e155cd0f9eec496
An An <2236368544@qq.com>
2022-05-15 16:34:30 +0800

    Update AALang.dart

--- lib/AAChartsLib/AAOptionsModel/AALang.dart
+++ lib/AAChartsLib/AAOptionsModel/AALang.dart
@@ -1,8 +1,28 @@
+import 'dart:convert';
+
 import 'package:aacharts_flutter/AAChartsLib/AAOptionsModel/AAObject.dart';
 
 class AALang extends AAObject {
+  String noData;
+  int numericSymbolMagnitude;
+  List<String> numericSymbols;
   String resetZoom;
   String thousandsSep;
+
+  AALang noDataSet(String prop) {
+    noData = prop;
+    return this;
+  }
+
+  AALang numericSymbolMagnitudeSet(int prop) {
+    numericSymbolMagnitude = prop;
+    return this;
+  }
+
+  AALang numericSymbolsSet(List<String> prop) {
+    numericSymbols = prop;
+    return this;
+  }
 
   AALang resetZoomSet(String prop) {
     resetZoom = prop;
@@ -16,6 +36,9 @@ class AALang extends AAObject {
 
   Map<String, dynamic> toJson() {
     return {
+      "noData": this.noData,
+      "numericSymbolMagnitude": this.numericSymbolMagnitude,
+      "numericSymbols": jsonEncode(this.numericSymbols),
       "resetZoom": this.resetZoom,
       "thousandsSep": this.thousandsSep,
     };
