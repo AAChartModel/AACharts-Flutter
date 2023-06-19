c0be1c652fea2cf3fcbbaa3e83fac853eae3c818
An An <2236368544@qq.com>
2023-06-19 17:16:07 +0800

    Update MixedChartPage.dart

--- lib/ChartsDemo/Page/MixedChartPage.dart
+++ lib/ChartsDemo/Page/MixedChartPage.dart
@@ -4,6 +4,8 @@ import 'package:aacharts_flutter/ChartsDemo/ChartComposer/MixedChartComposer.dar
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../AAChartsLib/AAChartCreator/AAChartView2.dart';
+
 class MixedChartPage extends StatelessWidget {
   final String selectedType;
 
@@ -12,12 +14,52 @@ class MixedChartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var aaChartView = AAChartView();
+
+    /*Mixed Chart*/
+    List chartTypeNamesArr = [
+      "Arearange Mixed Line---面积范围均线图",
+      "Columnrange Mixed Line---柱形范围图混合折线图",
+      "Stacking Column Mixed Line---堆积柱形图混合折线图",
+      "Dash Style Types Mixed---多种类型曲线混合图",
+      "Negative Color Mixed Column Chart---基准线以下异色混合图",
+      "scatterMixedLine---散点图混合折线图",
+      "Negative Color Mixed Bubble Chart---基准线以下异色气泡图",
+      "Polygon Mixed Scatter---多边形混合散点图",
+      "Polar Chart Mixed---极地混合图",
+      "Column Mixed Scatter---柱形图混合散点图",
+      "Pie Mixed Line Mixed Column---扇形折线柱形混合图",
+      "Line Chart With Shadow---带有阴影效果の折线图",
+      "Negative Color Mixed Areaspline chart---基准线以下异色混合曲线填充图",
+      "Aerasplinerange Mixed Columnrange Mixed Line Chart---曲线面积范围混合柱形范围混合折线图"
+    ];
+
+    /*Mixed Chart*/
+    List chartTypesArr = [
+      "arearangeMixedLine",
+      "columnrangeMixedLine",
+      "stackingColumnMixedLine",
+      "dashStyleTypeMixed",
+      "negativeColorMixed",
+      "scatterMixedLine",
+      "negativeColorMixedBubble",
+      "polygonMixedScatter",
+      "polarChartMixed",
+      "columnMixedScatter",
+      "PieMixedLineMixedColumn",
+      "LineChartWithShadow",
+      "NegativeColorMixedAreasplineChart",
+      "AerasplinerangeMixedColumnrangeMixedLineChart"
+    ];
+
+    var aaChartView = AAChartView2();
 
     var aaChartModel = configureChartModelWithChartType(selectedType);
     aaChartView.aa_drawChartWithChartModel(aaChartModel);
 
     return Scaffold(
+      appBar: AppBar(
+        title: Text('SpecialChart'),
+      ),
       body: GestureDetector(
         onTap: () {
           Navigator.pop(context);
