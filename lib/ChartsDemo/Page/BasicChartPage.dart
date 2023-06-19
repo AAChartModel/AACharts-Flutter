94824757d8cf071eb24c036909c7027494e17114
An An <2236368544@qq.com>
2023-06-19 17:15:54 +0800

    Update BasicChartPage.dart

---
+++ lib/ChartsDemo/Page/BasicChartPage.dart
@@ -0,0 +1,46 @@
+
+import 'package:aacharts_flutter/AAChartsLib/AAChartCreator/AAChartModel.dart';
+import 'package:aacharts_flutter/AAChartsLib/AAChartCreator/AAChartView.dart';
+import 'package:aacharts_flutter/ChartsDemo/ChartComposer/SpecialChartComposer.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BasicChartPage extends StatelessWidget {
+  final String selectedType;
+
+  const BasicChartPage({ Key? key, required this.selectedType}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    var aaChartView = AAChartView();
+
+    var aaChartModel = configureChartModelWithChartType(selectedType);
+    aaChartView.aa_drawChartWithChartModel(aaChartModel);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BasicChart'),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Center(
+          child: Hero(
+              tag: 'imageHero',
+              child: aaChartView
+          ),
+        ),
+      ),
+    );
+  }
+
+  AAChartModel configureChartModelWithChartType(String chartType) {
+    switch (chartType) {
+
+      default: return SpecialChartComposer.configureColumnChart();
+    }
+  }
+
+}
