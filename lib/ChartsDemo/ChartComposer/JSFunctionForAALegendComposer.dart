2e046d70def0346bca7cf5b9daba5e26e77b66b1
An An <2236368544@qq.com>
2023-06-27 17:57:02 +0800

    Update JSFunctionForAALegendComposer.dart

--- lib/ChartsDemo/ChartComposer/JSFunctionForAALegendComposer.dart
+++ lib/ChartsDemo/ChartComposer/JSFunctionForAALegendComposer.dart
@@ -3,6 +3,7 @@ import 'package:aacharts_flutter/AAChartsLib/AAChartCreator/AAColor.dart';
 import 'package:aacharts_flutter/AAChartsLib/AAChartCreator/AAGradientColor.dart';
 import 'package:aacharts_flutter/AAChartsLib/AAOptionsModel/AAItemStyle.dart';
 import 'package:aacharts_flutter/AAChartsLib/AAOptionsModel/AASeries.dart';
+import 'package:aacharts_flutter/AAChartsLib/AAOptionsModel/AAStates.dart';
 
 import '../../AAChartsLib/AAChartCreator/AASeriesElement.dart';
 import '../../AAChartsLib/AAOptionsModel/AAOptions.dart';
@@ -71,4 +72,96 @@ class JSFunctionForAALegendComposer {
 
     return aaOptions;
   }
+
+  // //https://github.com/AAChartModel/AAChartKit-Swift/issues/391
+//     //https://github.com/AAChartModel/AAChartKit-Swift/issues/393
+//     private func disableLegendClickEventForPieChart() -> AAOptions {
+//          let aaChartModel = AAChartModel()
+//             .chartType(.pie)
+//             .backgroundColor(AAColor.white)
+//             .title("LANGUAGE MARKET SHARES JANUARY,2020 TO MAY")
+//             .subtitle("virtual data")
+//             .dataLabelsEnabled(true)//是否直接显示扇形图数据
+//             .yAxisTitle("℃")
+//             .series([
+//                 AASeriesElement()
+//                     .name("Language market shares")
+//                     .innerSize("20%")//内部圆环半径大小占比(内部圆环半径/扇形图半径),
+//                     .allowPointSelect(true)
+//                     .states(AAStates()
+//                         .hover(AAHover()
+//                             .enabled(false)//禁用点击区块之后出现的半透明遮罩层
+//                     ))
+//                     .data([
+//                         ["Java"  ,67],
+//                         ["Swift",999],
+//                         ["Python",83],
+//                         ["OC"    ,11],
+//                         ["Go"    ,30],
+//                     ])
+//             ])
+//
+//         let aaOptions = aaChartModel.aa_toAAOptions()
+//         aaOptions.legend?.labelFormat("{name} {percentage:.2f}%")
+//
+//         //禁用饼图图例点击事件
+//         aaOptions.plotOptions?.series?
+//             .point(AAPoint()
+//                 .events(AAPointEvents()
+//                     .legendItemClick(#"""
+//                     function() {
+//                       return false;
+//                     }
+//         """#)))
+//
+//         return aaOptions
+//     }
+
+//https://github.com/AAChartModel/AAChartKit-Swift/issues/391
+//https://github.com/AAChartModel/AAChartKit-Swift/issues/393
+static disableLegendClickEventForPieChart() {
+    var aaChartModel = AAChartModel()
+        .chartTypeSet(AAChartType.pie)
+        .backgroundColorSet(AAColor.white)
+        .titleSet("LANGUAGE MARKET SHARES JANUARY,2020 TO MAY")
+        .subtitleSet("virtual data")
+        .dataLabelsEnabledSet(true)//是否直接显示扇形图数据
+        .yAxisTitleSet("℃")
+        .seriesSet([
+      AASeriesElement()
+          .nameSet("Language market shares")
+          .innerSizeSet("20%")//内部圆环半径大小占比(内部圆环半径/扇形图半径),
+          .allowPointSelectSet(true)
+          .statesSet(
+          AAStates()
+              .hoverSet(
+              AAHover()
+                  .enabledSet(false)//禁用点击区块之后出现的半透明遮罩层
+          ))
+          .dataSet([
+        ["Java", 67],
+        ["Swift", 999],
+        ["Python", 83],
+        ["OC", 11],
+        ["Go", 30],
+      ])
+    ]);
+
+    var aaOptions = aaChartModel.aa_toAAOptions();
+    aaOptions.legend
+        ?.labelFormatSet("{name} {percentage:.2f}%");
+
+    //禁用饼图图例点击事件
+    aaOptions.plotOptions?.series?.pointSet(
+      AAPoint()
+          .eventsSet(
+          AAPointEvents()
+
+
+
+      )
+    );
+
+    return aaOptions;
+}
 }
