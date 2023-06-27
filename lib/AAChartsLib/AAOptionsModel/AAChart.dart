36ef510b64910b18eebbd513e47ce9f9b9ad3f4e
An An <2236368544@qq.com>
2023-06-27 14:37:40 +0800

    Add AAChartEvents for AAChart

--- lib/AAChartsLib/AAOptionsModel/AAChart.dart
+++ lib/AAChartsLib/AAOptionsModel/AAChart.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:aacharts_flutter/AAChartsLib/AATools/AAEasyTool.dart';
+
 import 'AAObject.dart';
 import 'AAAnimation.dart';
 import 'AAResetZoomButton.dart';
@@ -22,6 +24,10 @@ class AAChart extends AAObject  {
   double? marginLeft;
   AAScrollablePlotArea? scrollablePlotArea;
   AAResetZoomButton? resetZoomButton;
+  String? zoomType;
+  AAChartEvents? events;
+  Object? height;
+
 
   AAChart typeSet(String? prop) {
     type = prop;
@@ -103,6 +109,21 @@ class AAChart extends AAObject  {
     return this;
   }
 
+  AAChart zoomTypeSet(String? prop) {
+    zoomType = prop;
+    return this;
+  }
+
+  AAChart eventsSet(AAChartEvents? prop) {
+    events = prop;
+    return this;
+  }
+
+  AAChart heightSet(Object? prop) {
+    height = prop;
+    return this;
+  }
+
   Map<String, dynamic> toJson() {
     return {
       "type": this.type,
@@ -121,7 +142,61 @@ class AAChart extends AAObject  {
       "marginLeft": this.marginLeft,
       "scrollablePlotArea": this.scrollablePlotArea == null ? null : this.scrollablePlotArea?.toPureJson(),
       "resetZoomButton": this.resetZoomButton == null ? null : this.resetZoomButton?.toPureJson(),
+      "zoomType": this.zoomType,
+      "events": this.events == null ? null : this.events?.toPureJson(),
+      "height": this.height,
     };
+  }
+
+}
+
+
+class AAChartEvents extends AAObject {
+
+  // public var click: String?
+  //     public var load: String?
+  //     public var redraw: String?
+  //     public var render: String?
+  //     public var selection: String?
+  String? click;
+  String? load;
+  String? redraw;
+  String? render;
+  String? selection;
+
+  AAChartEvents clickSet(String? prop) {
+    if (prop != null) {
+      click = AAEasyTool.pureJavaScriptFunctionString(prop);
+    }
+    return this;
+  }
+
+  AAChartEvents loadSet(String? prop) {
+    if (prop != null) {
+      load = AAEasyTool.pureJavaScriptFunctionString(prop);
+    }
+    return this;
+  }
+
+  AAChartEvents redrawSet(String? prop) {
+    if (prop != null) {
+      redraw = AAEasyTool.pureJavaScriptFunctionString(prop);
+    }
+    return this;
+  }
+
+  AAChartEvents renderSet(String? prop) {
+    if (prop != null) {
+      render = AAEasyTool.pureJavaScriptFunctionString(prop);
+    }
+    return this;
+  }
+
+  AAChartEvents selectionSet(String? prop) {
+    if (prop != null) {
+      selection = AAEasyTool.pureJavaScriptFunctionString(prop);
+    }
+    return this;
   }
 
 }
